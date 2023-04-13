18d543a2f6860297d179fd203d2b6b4659479f39
Elyas <elyassalar8@gmail.com>
2023-04-13 15:07:59 +0300

    fix: responsiveness of the location UI

---
+++ shared/constants/media-query.ts
@@ -0,0 +1,6 @@
+export const MEDIA_QUERIES = {
+  MOBILE: "(max-width: 320px)",
+  TABLET: "(max-width: 768px)",
+  DESKTOP: "(max-width: 1024px)",
+  LARGE_DESKTOP: "(max-width: 1280px)",
+} as const
---
+++ shared/hooks/useMediaQuery.ts
@@ -0,0 +1,39 @@
+import { useCallback, useEffect, useState } from "react"
+
+const useMediaQuery = (query: string): boolean => {
+  const getMatches = (query: string): boolean => {
+    if (typeof window !== 'undefined') {
+      return window.matchMedia(query).matches
+    }
+    return false
+  }
+
+  const [matches, setMatches] = useState<boolean>(getMatches(query))
+
+  const handleChange = useCallback(() => {
+    setMatches(getMatches(query))
+  }, [query])
+
+  useEffect(() => {
+    const matchMedia = window.matchMedia(query)
+    handleChange()
+
+    if (matchMedia.addListener) {
+      matchMedia.addListener(handleChange)
+    } else {
+      matchMedia.addEventListener('change', handleChange)
+    }
+
+    return () => {
+      if (matchMedia.removeListener) {
+        matchMedia.removeListener(handleChange)
+      } else {
+        matchMedia.removeEventListener('change', handleChange)
+      }
+    }
+  }, [query, handleChange])
+
+  return matches
+}
+
+export default useMediaQuery
