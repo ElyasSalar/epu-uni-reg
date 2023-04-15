f60916600624f387455d541f370d69f35252c471
Elyas <elyassalar8@gmail.com>
2023-04-16 00:00:53 +0300

    fix: translations and design issues

--- shared/constants/instructions.ts
+++ shared/constants/instructions.ts
@@ -3,22 +3,22 @@ import { ROUTES } from "./routes";
 export const INSTRUCTIONS = [
   {
     href: ROUTES.instructions.howToRegister.path,
-    title: "How to get registered",
-    description: "Students can register through university's online system.Students can register through university's online system."
+    title: "instructions_register_title",
+    description: "instructions_register_brief"
   },
   {
     href: ROUTES.instructions.howToRegister.path,
-    title: "How to get registered",
-    description: "Students can register through university's online system.Students can register through university's online system."
+    title: "instructions_register_title",
+    description: "instructions_register_brief"
   },
   {
     href: ROUTES.instructions.howToRegister.path,
-    title: "How to get registered",
-    description: "Students can register through university's online system.Students can register through university's online system."
+    title: "instructions_register_title",
+    description: "instructions_register_brief"
   },
   {
     href: ROUTES.instructions.howToRegister.path,
-    title: "How to get registered",
-    description: "Students can register through university's online system.Students can register through university's online system."
+    title: "instructions_register_title",
+    description: "instructions_register_brief"
   },
 ]
