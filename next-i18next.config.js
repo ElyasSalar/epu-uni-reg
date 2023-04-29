899e435414e792ba3ebc7c82ea2b61d2648eefbb
Elyas <elyassalar8@gmail.com>
2023-04-29 20:41:24 +0300

    fix: resolve locale path configuration

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,8 +1,11 @@
+const path = require("path")
+
 module.exports = {
   i18n: {
     locales: ["ckb", "en"],
     localeDetection: false,
     defaultLocale: "ckb",
+    localePath: path.resolve('./public/locales')
   },
   returnNull: false,
 }
